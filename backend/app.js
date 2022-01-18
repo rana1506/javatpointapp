de306e15293d55d5eab4f714da3024cce08d1ce8
rana1506 <rana1506@gmail.com>
2022-01-18 09:59:28 +0600

    update working

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use((req, res, next)=>{
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader("Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS");
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS");
   next();
 });
 
